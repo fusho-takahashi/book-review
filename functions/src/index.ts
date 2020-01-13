4cd18f068115f80e39da3eb2b8afe415184e009f
fusho-takahashi <zenryaku@fusho-takahashi.me>
2020-01-13 09:23:30 +0900

    functionを定義

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,44 @@
 import * as functions from 'firebase-functions';
+import * as request from 'request';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const db = admin.firestore();
+
+exports.isbn = functions.https.onCall((data, context) => {
+  const opt = {
+    url: `http://www.googleapis.com/books/v1/volumes?country=JP&q=${data.isbn}`,
+    method: 'GET',
+    json: true,
+  };
+  request(opt, async (error, response, body) => {
+    if (body.item.length === 0) {
+      return 'ERR';
+    }
+    const gid = body.items[0]['id'];
+    const item = body.items[0].volumeInfo;
+    let isbn10 = 'undefined';
+    if (item.industryIdentifers[0]['type'] === 'ISBN_10') {
+      isbn10 = item.industryIdentifers[0]['identifer'];
+    }
+    if (item.industryIdentifers[1]['type'] === 'ISBN_10') {
+      isbn10 = item.industryIdentifers[1]['identifer'];
+    }
+    const obj = {
+      isbn: data.isbn,
+      gid: gid,
+      isbn10: isbn10,
+      author: item.authors === undefined ? 'undefined' : item.authors[0],
+      publisher: item.publisher === undefined ? 'undefined' : item.publisher,
+      title: item.title === undefined ? 'undefined' : item.title,
+      subtitle: item.subtitle === undefined ? 'undefined' : item.subtitle,
+      published: item.publishedDate === undefined ? 'undefined' : item.publishedDate,
+      desc: item.description === undefined ? 'undefined' : item.description,
+      created: new Date().getTime(),
+    };
+
+    await db.collection('books').add(obj);
+    return 'OK';
+  });
+});
